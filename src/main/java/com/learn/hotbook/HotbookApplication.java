801caf6f0a1a08d53625c2cdf50bf3f178933551
chenpeng <chen_peng_cp@126.com>
2020-07-22 22:57:39 +0800

    initialize

---
+++ src/main/java/com/learn/hotbook/HotbookApplication.java
@@ -0,0 +1,13 @@
+package com.learn.hotbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotbookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotbookApplication.class, args);
+    }
+
+}
